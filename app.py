4c9e47e44d952f31c082862c34151bac6e03c633
Nicola Martinez <nicoenrique_1994@hotmail.com>
2021-10-10 15:23:25 -0300

    auth

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS, cross_origin
 from models import db, User
 from flask_migrate import Migrate
 
@@ -7,15 +8,28 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost:5432/warezgeeks'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['DEBUG'] = True
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 db.init_app(app)
 Migrate(app, db)
 
 
 @app.route("/login", methods=["POST"])
+@cross_origin()
 def login():
-    user = User.query.get(1)
-    return jsonify(user.serialize_just_username())
+    request.get_json(force=True)
+    print(request.json)
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    # Query your database for username and password
+    user = User.query.filter_by(email=email, password=password).first()
+    if user is None:
+        # the user was not found on the database
+        return jsonify({"msg": "Bad username or password"}), 401
+    # create a new token with the user id inside
+    return jsonify({ "msg": "Logged in succesfully" })
+    
 
 if __name__ == '__main__':
     app.run(host='localhost', port=8080)
